10584f9e6e98f01069ba44a57da5698a61c67ac9
SungChoi <swc1098@rit.edu>
2015-04-11 14:16:10 -0400

    Added Player Movement.
    It works by getting the axis of the object, and then altering it's position. I'll fiddle with it later to make it less floaty, but I kinda like it.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,14 +2,46 @@
 using System.Collections;
 
 public class CharacterController : MonoBehaviour {
+    // get the speed and acceleration of object
+    public float maxSpeed = 10;
+    public float acceleration = 3;
+
+    // grab Object's positions.
+    public float posX;
+    public float posY;
 
 	// Use this for initialization
 	void Start () {
-	
+	       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Movement();
 	}
+
+    private void Movement()
+    {
+        posX = Input.GetAxis("Horizontal");
+        posY = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector2(posX * maxSpeed, posY * maxSpeed);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x, maxSpeed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2D.AddForce(new Vector2(-maxSpeed, rigidbody2D.velocity.y));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x, -maxSpeed));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2D.AddForce(new Vector2(maxSpeed, rigidbody2D.velocity.y));
+        }
+    }
 }
