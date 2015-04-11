5ff57a0065194a9d879521e54c76b62c41c51d12
SungChoi <swc1098@rit.edu>
2015-04-11 15:36:43 -0400

    Fixed up Movement Script.
    Friction is still a little weird, but it shouldn't be as floaty as before.
    Gonna try and make stopping a little less sudden for tuesday.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+    // get the speed and acceleration of object
+    public float maxSpeed = 10;
+    public float speed = 5.0f;
+
+    // grab Object's positions.
+    public float posX;
+    public float posY;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    // Handles all of the movement. (Called in Update)
+    private void Movement()
+    {
+        // get position.
+        posX = Input.GetAxis("Horizontal");
+        posY = Input.GetAxis("Vertical");
+
+        Vector2 move = (new Vector2(posX, posY).normalized) * speed;
+        Vector2 slow = (new Vector2(posX, posY).normalized) * -speed;
+        rigidbody2D.AddForce(move, ForceMode2D.Impulse);
+
+        if (move == Vector2.zero)
+        {
+            rigidbody2D.velocity = slow;
+        }
+        if (Mathf.Abs(rigidbody2D.velocity.x) >= maxSpeed)
+        {
+            rigidbody2D.velocity = new Vector2(maxSpeed * rigidbody.velocity.x, rigidbody.velocity.y);
+        }
+        if (Mathf.Abs(rigidbody2D.velocity.y) >= maxSpeed)
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody.velocity.x, maxSpeed * rigidbody.velocity.y);
+        }
+    }
+}
