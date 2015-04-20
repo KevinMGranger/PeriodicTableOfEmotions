e6b6a3d54c9384e1cde48ba7cbfe1a3b310afc01
Kevin M Granger <kmg2728@rit.edu>
2015-04-20 14:41:55 -0400

    Electrons!

---
+++ Assets/Scripts/TawkToMe.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TawkToMe : MonoBehaviour {
+
+	public GameObject talkBubble;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name == "Player")
+		{
+			talkBubble.SetActive(true);
+		}
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.gameObject.name == "Player")
+		{
+			talkBubble.SetActive(false);
+		}
+	}
+}
