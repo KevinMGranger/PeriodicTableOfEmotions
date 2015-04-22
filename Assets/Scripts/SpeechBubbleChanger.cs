f027d95cd716dee154b64587f66da85381a3bd33
Kevin M Granger <kmg2728@rit.edu>
2015-04-22 11:46:42 -0400

    Add in speech bubbles and a demo of switching them

---
+++ Assets/Scripts/SpeechBubbleChanger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Shader))]
+public class SpeechBubbleChanger : MonoBehaviour {
+
+	public Material blank, ellipsis, exclaim, heart;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.U))
+		{
+			if (renderer.sharedMaterial == exclaim)
+			{
+				renderer.sharedMaterial = ellipsis;
+			}
+			else
+			{
+				renderer.sharedMaterial = exclaim;
+			}
+		}
+	}
+}
