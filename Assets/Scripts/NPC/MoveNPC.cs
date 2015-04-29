224da825ea3fbc9f5d34a4f53f235f951a918d34
SungChoi <swc1098@rit.edu>
2015-04-29 15:08:18 -0400

    NPC Carry / Event Triggers / Commenting
    NPC is now treated as child when carried, no longer follows you.
    Replaced a bunch of stuff that used to rely on Update with Event Triggers. (Undetermined if correct way of handling it.)
    Commented a lot of the stuff that were left uncommented.

--- Assets/Scripts/NPC/MoveNPC.cs
+++ Assets/Scripts/NPC/MoveNPC.cs
@@ -5,32 +5,28 @@ using System.Collections;
 public class MoveNPC : MonoBehaviour {
 	public Character player;
 	public TawkToMe ttm;
+
+    // a bool meant to control the event's invoke. 
+    public bool isMoving = false;
+
+    [ContextMenu("Is the object following the player?")]
     public UnityEvent followMe;
-	public bool isMoving = false;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// check for input
-		if (Input.GetKeyDown (KeyCode.F) && isMoving == false && ttm.isColliding == true) {
-			isMoving = true;
-		} else if (Input.GetKeyDown (KeyCode.G) && isMoving == true) {
-			isMoving = false;
-		}
-		if (isMoving == true) {
-            followMe.Invoke();
-		}
-
+        InvokeFollow();
 	}
     /// <summary>
     /// If the distance between the player and the gameObject is too large, transform the gameObject towards the player. 
     /// </summary>
-	void followPlayer()
+	public void followPlayer()
 	{
-		// Follow the player here
+		// This object's parent is the player.
+        this.transform.parent = player.gameObject.transform;
+        /*
 		if((Vector3.Distance(this.transform.position, player.gameObject.transform.position)) > 1.7f)
 		{
 			Quaternion startingRotation = transform.rotation;
@@ -38,9 +34,33 @@ public class MoveNPC : MonoBehaviour {
 			transform.Translate(Vector3.forward * 0.1f);
 			transform.rotation = startingRotation;
 		}
+         */
 	}
+    void InvokeFollow()
+    {
+        // If the F key is pressed, the player will carry the object
+		if (Input.GetKeyDown(KeyCode.F) && isMoving == false && ttm.isColliding == true) {
+			isMoving = true;
+        }
+        // if the G key is pressed, the player will release the object.
+        else if (Input.GetKeyDown(KeyCode.G) && isMoving == true)
+        {
+			isMoving = false;
+		}
+        // This will invoke the event as long as the player pressed the F key. 
+        if (isMoving == true)
+        {
+            followMe.Invoke();
+        }
+        // if the player releases the object, isMoving is then false so the object is allowed to stop. 
+        else
+        {
+            this.transform.parent = null;
+            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+    }
     /// <summary>
-    /// 
+    /// Checks to see if a match was made between two atoms
     /// </summary>
     void checkMatch()
     {
